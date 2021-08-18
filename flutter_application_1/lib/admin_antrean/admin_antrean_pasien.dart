bdf95e9a5d43af3b9cce9e6499961c78227b8819
mlatifr <mlatifraldi@gmail.com>
2021-08-18 13:24:49 +0700

    hlm admin antrean pasien: hapus antrean

--- flutter_application_1/lib/admin_antrean/admin_antrean_pasien.dart
+++ flutter_application_1/lib/admin_antrean/admin_antrean_pasien.dart
@@ -39,43 +39,94 @@ class _AdminAntreanPasienState extends State<AdminAntreanPasien> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Antrean Pasien")),
-        ),
-        drawer: widgetDrawer(),
-        body: ListView(
-          children: [
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        child: TextFormField(
-                      enabled: false,
-                      decoration: InputDecoration(
-                        labelText: 'Tanggal Lahir',
-                        fillColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: Colors.blue,
-                          ),
-                        ),
-                      ),
-                    )),
-                    ElevatedButton(
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.calendar_today_sharp,
-                          color: Colors.white,
-                          size: 24.0,
-                        ))
-                  ],
-                )),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Center(child: Text("Antrean Pasien")),
+          ),
+          drawer: widgetDrawer(),
+          // body: ListView(
+          //   children: [
+          //     Padding(
+          //         padding: EdgeInsets.all(10),
+          //         child: Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           children: [
+          //             Expanded(
+          //                 child: TextFormField(
+          //               enabled: false,
+          //               decoration: InputDecoration(
+          //                 labelText: 'Tanggal Lahir',
+          //                 fillColor: Colors.white,
+          //                 enabledBorder: OutlineInputBorder(
+          //                   borderRadius: BorderRadius.circular(10.0),
+          //                   borderSide: BorderSide(
+          //                     color: Colors.blue,
+          //                   ),
+          //                 ),
+          //               ),
+          //             )),
+          //             ElevatedButton(
+          //                 onPressed: () {},
+          //                 child: Icon(
+          //                   Icons.calendar_today_sharp,
+          //                   color: Colors.white,
+          //                   size: 24.0,
+          //                 ))
+          //           ],
+          //         )),
+          //   ],
+          // ),
+          body: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  onDismissed: (direction) {
+                    if (direction == DismissDirection.endToStart) {
+                      print('end to start');
+                    } else {
+                      print('else');
+                    }
+                  },
+                  confirmDismiss: (direction) {
+                    return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Konfirmasi'),
+                            content:
+                                Text('Apakah ingin menghapus antrian ini?'),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(true);
+                                  },
+                                  child: Text('Yes')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(false);
+                                  },
+                                  child: Text('No')),
+                            ],
+                          );
+                        });
+                  },
+                  key: Key(index.toString()),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.delete,
+                      size: 25,
+                    ),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 10),
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(),
+                    title: Text('${index + 1}'),
+                    subtitle: Text('sub judul'),
+                  ),
+                );
+              })),
     );
   }
 }
