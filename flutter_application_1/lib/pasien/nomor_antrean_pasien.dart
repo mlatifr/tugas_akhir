89cafd249e46016a2ea200135df3053de059ed7d
mlatifr <mlatifraldi@gmail.com>
2021-10-25 05:40:16 +0700

    PsienNoAntr:pre-FutreBulder

--- flutter_application_1/lib/pasien/nomor_antrean_pasien.dart
+++ flutter_application_1/lib/pasien/nomor_antrean_pasien.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:http/http.dart' as http;
+
+class PsienVNoAntr {
+  var id_user, no_antre, tgl_visit;
+  PsienVNoAntr({this.id_user, this.no_antre, this.tgl_visit});
+
+  // untuk convert dari jSon
+  factory PsienVNoAntr.fromJson(Map<String, dynamic> json) {
+    return new PsienVNoAntr(
+      id_user: json['id_user'],
+      no_antre: json['no_antre'],
+      tgl_visit: json['tgl_visit'],
+    );
+  }
+}
+
+List<PsienVNoAntr> PVAs = [];
 
 class AntreanPasien extends StatefulWidget {
-  var nomor_antrean, antrean_sekarang;
-  AntreanPasien({Key key, this.nomor_antrean, this.antrean_sekarang})
+  final user_klinik_id, tgl_visit;
+  AntreanPasien({Key key, this.user_klinik_id, this.tgl_visit})
       : super(key: key);
 
   @override
@@ -10,8 +30,39 @@ class AntreanPasien extends StatefulWidget {
 }
 
 class _AntreanPasienState extends State<AntreanPasien> {
+  Future<String> fetchDataTglVstPsien() async {
+    final response = await http
+        .post(Uri.parse(APIurl + "pasien_view_antrean_user_tgl.php"), body: {
+      'user_klinik_id': widget.user_klinik_id.toString(),
+      'tgl_visit': widget.tgl_visit.toString()
+    });
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to read API');
+    }
+  }
+
+  bacaDataTglVstPsien() {
+    Future<String> data = fetchDataTglVstPsien();
+    data.then((value) {
+      // ignore: unused_local_variable
+      setState(() {
+        Map json = jsonDecode(value);
+        if (json['result'].toString() == 'success') {
+          for (var i in json['data']) {
+            PsienVNoAntr pva = PsienVNoAntr.fromJson(i);
+            PVAs.add(pva);
+          }
+        } else {}
+        setState(() {});
+      });
+    });
+  }
+
   @override
   void initState() {
+    PVAs.clear();
     super.initState();
   }
 
@@ -40,7 +91,7 @@ class _AntreanPasienState extends State<AntreanPasien> {
             )),
             Center(
                 child: Text(
-              widget.nomor_antrean.toString(),
+              widget.user_klinik_id.toString(),
               style: TextStyle(fontSize: 88, color: Colors.blueAccent),
               textAlign: TextAlign.center,
             )),
@@ -52,7 +103,7 @@ class _AntreanPasienState extends State<AntreanPasien> {
             )),
             Center(
                 child: Text(
-              widget.antrean_sekarang.toString(),
+              widget.tgl_visit.toString(),
               style: TextStyle(color: Colors.black38, fontSize: 50),
               textAlign: TextAlign.center,
             ))
