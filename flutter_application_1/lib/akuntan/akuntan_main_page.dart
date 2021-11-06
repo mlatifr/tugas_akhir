ef68ddd6ae0c14dfed28925a4976e71da6a55047
mlatifr <mlatifraldi@gmail.com>
2021-11-06 11:15:36 +0700

    formatPOstAmanTetapiTidakMengirimApaApa

--- flutter_application_1/lib/akuntan/akuntan_main_page.dart
+++ flutter_application_1/lib/akuntan/akuntan_main_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/akuntan/akuntan_v_nota_penjualan.dart';
-
+import 'package:http/http.dart' as http;
 import '../main.dart';
 
 class AkuntanMainPage extends StatefulWidget {
@@ -50,6 +53,65 @@ class _AkuntanMainPageState extends State<AkuntanMainPage> {
     );
   }
 
+  var transaksi_array = {
+    'transaksi_array': {
+      'transaksi_1': {
+        'penjurnalan_id': '1',
+        'daftar_akun_id': '4',
+        'tgl_catat': '2021-10-01',
+        'debet': '1000000',
+        'kredit': '',
+        'ket_transaksi': 'pendapatan jasa medis'
+      },
+      'transaksi_2': {
+        'penjurnalan_id': '1',
+        'daftar_akun_id': '1',
+        'tgl_catat': '2021-10-01',
+        'debet': '',
+        'kredit': '1000000',
+        'ket_transaksi': 'pendapatan jasa medis'
+      },
+    }
+  };
+  Map<String, String> headerz = {
+    HttpHeaders.contentTypeHeader: "application/json"
+  };
+  Future<String> fetchDataAkuntanInputTransaksiPenjurnalan() async {
+    final response =
+        await http.post(Uri.parse(APIurl + "akuntan_inpt_penjurnalan_akun.php"),
+            headers: {
+              'Content-type': 'application/json',
+              "Accept": "application/json",
+            },
+            body: json.encode({
+              'transaksi_array': {
+                'transaksi_1': {
+                  'penjurnalan_id': '1',
+                  'daftar_akun_id': '4',
+                  'tgl_catat': '2021-10-01',
+                  'debet': '1000000',
+                  'kredit': '',
+                  'ket_transaksi': 'pendapatan jasa medis'
+                },
+                'transaksi_2': {
+                  'penjurnalan_id': '1',
+                  'daftar_akun_id': '1',
+                  'tgl_catat': '2021-10-01',
+                  'debet': '',
+                  'kredit': '1000000',
+                  'ket_transaksi': 'pendapatan jasa medis'
+                },
+              }
+            }));
+    // body: jsonEncode({transaksi_array}));
+    if (response.statusCode == 200) {
+      print('fetchDataAkuntanInputTransaksiPenjurnalan: ${response.body}');
+      return response.body;
+    } else {
+      throw Exception('Failed to read API');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,6 +124,15 @@ class _AkuntanMainPageState extends State<AkuntanMainPage> {
           drawer: widgetDrawer(),
           body: Column(
             children: [
+              Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        fetchDataAkuntanInputTransaksiPenjurnalan();
+                        // var jE = jsonEncode(transaksi_array);
+                        // var jD = jsonDecode(jE);
+                        // print(transaksi_array.toString());
+                      },
+                      child: Text('Ok')))
               // widgetSelectTgl(),
               // widgetLsTile(),
             ],
